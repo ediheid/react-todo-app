43f46abf8e786cf96509442db7871f017458d3af
Edith-Heidmann <eheidmann@gmail.com>
2021-11-29 14:01:40 +0100

    Updated Navigation and changed git remote origin from GitHub classroom to my personal account

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,13 +2,13 @@ import React from "react";
 import logo from "../images/logo.png";
 
 const Navigation = () => {
-    return (
-        <nav>
-            <div className="left">
-                <img src={logo} alt="Logo" width="125%" />
-            </div>
-        </nav>
-    );
-}
+  return (
+    <nav>
+      <div className="left">
+        <img src={logo} alt="Logo" width="125%" />
+      </div>
+    </nav>
+  );
+};
 
 export default Navigation;
